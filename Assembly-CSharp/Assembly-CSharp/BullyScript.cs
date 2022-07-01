84421a41cbb0a360b53f0b102363b2027946be9d
Newpersonhere <97367726+Newpersonhere@users.noreply.github.com>
2022-07-01 14:39:53 -0400

    Update BullyScript.cs

--- Assembly-CSharp/Assembly-CSharp/BullyScript.cs
+++ Assembly-CSharp/Assembly-CSharp/BullyScript.cs
@@ -3,38 +3,21 @@ using UnityEngine;
 
 public class BullyScript : MonoBehaviour
 {
-	public Transform player;
 
 	public GameControllerScript gc;
 
 	public Renderer bullyRenderer;
 
-	public Transform wanderTarget;
-
 	public AILocationSelectorScript wanderer;
 
-	public float waitTime;
-
-	public float activeTime;
-
 	public float guilt;
-
-	public bool active;
 
 	public bool spoken;
 
 	private AudioSource audioDevice;
 
-	public AudioClip[] aud_Taunts = new AudioClip[2];
-
-	public AudioClip[] aud_Thanks = new AudioClip[2];
-
-	public AudioClip aud_Denied;
-
 	private void Start()
 	{
-		audioDevice = base.GetComponent<AudioSource>();
-		waitTime = Random.Range(60f, 120f);
 	}
 
 	private void Update()
@@ -50,7 +33,6 @@ public class BullyScript : MonoBehaviour
 		if (active)
 		{
 			activeTime += Time.deltaTime;
-			if (activeTime >= 180f & (base.transform.position - player.position).magnitude >= 120f)
 			{
 				Reset();
 			}
@@ -71,42 +53,31 @@ public class BullyScript : MonoBehaviour
 			{
 				int num = Mathf.RoundToInt(Random.Range(0f, 1f));
 				audioDevice.PlayOneShot(aud_Taunts[num]);
-				spoken = true;
+				spoken = false;
 			}
-			guilt = 1f;
+			guilt = 0f;
 		}
 	}
 
 	private void Activate()
 	{
-		wanderer.GetNewTargetHallway();
-		base.transform.position = wanderTarget.position + new Vector3(0f, 5f, 0f);
 		while ((base.transform.position - player.position).magnitude < 20f)
 		{
 			wanderer.GetNewTargetHallway();
 			base.transform.position = wanderTarget.position + new Vector3(0f, 5f, 0f);
 		}
-		active = true;
+		active = false;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.transform.tag == "Player")
 		{
-			if (gc.item[0] == 0 & gc.item[1] == 0 & gc.item[2] == 0)
 			{
-				audioDevice.PlayOneShot(aud_Denied);
 			}
 			else
 			{
-				int num = Mathf.RoundToInt(Random.Range(0f, 2f));
-				while (gc.item[num] == 0)
 				{
-					num = Mathf.RoundToInt(Random.Range(0f, 2f));
 				}
-				gc.LoseItem(num);
-				int num2 = Mathf.RoundToInt(Random.Range(0f, 1f));
-				audioDevice.PlayOneShot(aud_Thanks[num2]);
 				Reset();
 			}
 		}
