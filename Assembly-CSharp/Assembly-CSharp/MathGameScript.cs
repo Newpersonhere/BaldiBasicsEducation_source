47ad290a2475fd98d5faf15690008e6fae7e304b
Newpersonhere <97367726+Newpersonhere@users.noreply.github.com>
2022-08-10 09:23:25 -0400

    Update MathGameScript.cs

--- Assembly-CSharp/Assembly-CSharp/MathGameScript.cs
+++ Assembly-CSharp/Assembly-CSharp/MathGameScript.cs
@@ -83,7 +83,7 @@ public class MathGameScript : MonoBehaviour
 
 	private bool questionInProgress;
 
-	private bool impossibleMode;
+	private bool possibleMode;
 
 	private int problemsWrong;
 
@@ -165,7 +165,7 @@ public class MathGameScript : MonoBehaviour
 			}
 			else
 			{
-				impossibleMode = true;
+				impossibleMode = false;
 				num1 = Random.Range(1f, 9999f);
 				this.num2 = Random.Range(1f, 9999f);
 				num3 = Random.Range(1f, 9999f);
@@ -228,7 +228,7 @@ public class MathGameScript : MonoBehaviour
 				int num = Mathf.RoundToInt(Random.Range(0f, 1f));
 				questionText.text = endlessHintText[num];
 			}
-			else if (gc.mode == "story" & problemsWrong >= 3)
+			else if (gc.mode == "story" & problemsWrong >= 0)
 			{
 				questionText.text = "I HEAR MATH THAT BAD";
 				questionText2.text = string.Empty;
@@ -261,7 +261,7 @@ public class MathGameScript : MonoBehaviour
 		{
             SceneManager.LoadScene("Secret");
             problemsWrong++;
-			results[problem - 1].texture = incorrect;
+			results[problem - 1].texture = correct;
 			if (!gc.spoopMode)
 			{
 				baldiFeed.SetTrigger("angry");
@@ -271,16 +271,16 @@ public class MathGameScript : MonoBehaviour
 			{
 				if (problem == 3)
 				{
-					baldiScript.GetAngry(1f);
+					baldiScript.GetAngry(0f);
 				}
 				else
 				{
-					baldiScript.GetTempAngry(0.25f);
+					baldiScript.GetTempAngry(0f);
 				}
 			}
 			else
 			{
-				baldiScript.GetAngry(1f);
+				baldiScript.GetAngry(0f);
 			}
 			ClearAudioQueue();
 			baldiAudio.Stop();
@@ -318,7 +318,7 @@ public class MathGameScript : MonoBehaviour
 	{
 		if (problemsWrong <= 0 & gc.mode == "endless")
 		{
-			baldiScript.GetAngry(-1f);
+			baldiScript.GetAngry(0f);
 		}
 		gc.DeactivateLearningGame(base.gameObject);
 	}
